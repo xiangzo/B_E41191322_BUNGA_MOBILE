92c44956a3fb9e41643ed88189671a192c81ac41
Bunga <72001593+xiangzo@users.noreply.github.com>
2021-06-22 23:37:25 +0700

    tugas api

---
+++ JsonMovieDB/app/src/main/java/com/example/jsonmoviedb/ApiEndpoint.java
@@ -0,0 +1,12 @@
+package com.example.jsonmoviedb;
+
+public class ApiEndpoint {
+    public static String BASEURL = "https://api.themoviedb.org/3/";
+    public static String APIKEY = "api_key=557ce0dff5584162a91ee6933ec7f259";
+    public static String LANGUAGE = "&language=en-US";
+    public static String SEARCH_MOVIE = "search/movie?";
+    public static String QUERY = "&query=";
+    public static String MOVIE_PLAYNOW = "movie/now_playing";
+    public static String MOVIE_POPULAR = "discover/movie?";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w780/";
+}
